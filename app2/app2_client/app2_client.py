3c40d6d3a07796195ace8bfaac214fc0a993e16a
Jeyhun Abbasov <abbcyhn@gmail.com>
2022-11-16 13:17:41 +0200

    Update app2

--- app2/app2_client/app2_client.py
+++ app2/app2_client/app2_client.py
@@ -1,5 +1,9 @@
+import os
 import requests
+from pydub import AudioSegment
 
+AUDIO_INPUT_PATH = "input/p001.mp3"
+SUBTITLE_INPUT_PATH = "input/p001.xhtml"
 FOG_API_ENDPOINT = "http://localhost:81/api/sync"
 CLOUD_API_ENDPOINT = "http://http://172.17.90.194:81/api/sync"
 
@@ -19,27 +23,35 @@ def readfile(filepath):
 		raise Exception('Error While Opening the file!')  
 
 def main():
-	index = 0
-	audio = readfile('input/p001.mp3')
-	subtitle = readfile('input/p001.xhtml')
+	subtitle = readfile(SUBTITLE_INPUT_PATH)
+	#audio = readfile(AUDIO_INPUT_PATH)
+	audio = AudioSegment.from_mp3(AUDIO_INPUT_PATH)
+	audio_slices = audio[::10000]
 
-	while(True):
-		
+	for index, chunk in enumerate(audio_slices):
 		response = "EMPTY RESPONSE"
+
+		# read audio slice
+		audio_slice_path = f'sound_{index}.mp3'		
+		with open(audio_slice_path, "wb") as f:
+			chunk.export(f, format="mp3")
+		audio_slice = readfile(audio_slice_path)
+
+		# read subtitle slice
+		## TODO
 
 		# send to fog
 		if (index % 2 == 0):
 			print("SENDING TO FOG")
-			response = sendto(audio, subtitle, FOG_API_ENDPOINT)
+			response = sendto(audio_slice, subtitle, FOG_API_ENDPOINT)
 			print("RETRIEVING FROM FOG")
 		# send to cloud
 		else:
 			print("SENDING TO CLOUD")
-			response = sendto(audio, subtitle, CLOUD_API_ENDPOINT)
+			response = sendto(audio_slice, subtitle, CLOUD_API_ENDPOINT)
 			print("RETRIEVING FROM CLOUD")
 
-		index += 1
-
+		#os.remove(audio_slice_path)
 		print(response)
 
 if __name__ == '__main__':
