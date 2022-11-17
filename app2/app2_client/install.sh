da9a51ea1da9d4fce27f0f6f999399c2c6aede07
Jeyhun Abbasov <abbcyhn@gmail.com>
2022-11-17 12:07:31 +0200

    Update install.sh in app2

--- app2/app2_client/install.sh
+++ app2/app2_client/install.sh
@@ -6,13 +6,13 @@ sudo apt install -y \
     wget
 
 # download input
-mkdir input
-cd input/
-FILEID="1uX_g9OQJE6L0ZD_1HO8hNPeZyqcARLzJ"
-FILENAME="traffic.mp4"
-FILEURL="https://docs.google.com/uc?export=download&id=$FILEID"
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate ${FILEURL} -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=$FILEID" -O $FILENAME && rm -rf /tmp/cookies.txt
-cd ..
+# mkdir input
+# cd input/
+# FILEID="1uX_g9OQJE6L0ZD_1HO8hNPeZyqcARLzJ"
+# FILENAME="traffic.mp4"
+# FILEURL="https://docs.google.com/uc?export=download&id=$FILEID"
+# wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate ${FILEURL} -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=$FILEID" -O $FILENAME && rm -rf /tmp/cookies.txt
+# cd ..
 
 # set virtualenv
 python3 -m pip install virtualenv
