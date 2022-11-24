d03ccbb361310e28040ed972447f50da1674532f
Jeyhun Abbasov <abbcyhn@gmail.com>
2022-11-24 07:54:58 +0200

    Update install.sh in app2

--- app2/app2_client/install.sh
+++ app2/app2_client/install.sh
@@ -9,12 +9,6 @@ sudo apt install -y \
 mkdir input
 cd input/
 
-# download pulp_fiction.mp4
-FILEID="1Rv7PuahO3wlWqvnyS81zPpG0TcpE1u3U"
-FILENAME="pulp_fiction.mp4"
-FILEURL="https://docs.google.com/uc?export=download&id=$FILEID"
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate ${FILEURL} -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=$FILEID" -O $FILENAME && rm -rf /tmp/cookies.txt
-
 # download pulp_fiction_audio.mp3
 FILEID="19LhfHev-QvszCwhQp-1TWjI90zpxGprM"
 FILENAME="pulp_fiction_audio.mp3"
