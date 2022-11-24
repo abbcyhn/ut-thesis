aece2c280f910a39e541a8f07400e7959017cc8d
Jeyhun Abbasov <abbcyhn@gmail.com>
2022-11-24 07:42:21 +0200

    Update install.sh in app2

--- app2/app2_client/install.sh
+++ app2/app2_client/install.sh
@@ -5,14 +5,24 @@ sudo apt install -y \
     python3-dev \
     wget
 
-# download input
-# mkdir input
-# cd input/
-# FILEID="1uX_g9OQJE6L0ZD_1HO8hNPeZyqcARLzJ"
-# FILENAME="traffic.mp4"
-# FILEURL="https://docs.google.com/uc?export=download&id=$FILEID"
-# wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate ${FILEURL} -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=$FILEID" -O $FILENAME && rm -rf /tmp/cookies.txt
-# cd ..
+# download inputs
+mkdir input
+cd input/
+
+# download pulp_fiction.mp4
+FILEID="1Rv7PuahO3wlWqvnyS81zPpG0TcpE1u3U"
+FILENAME="pulp_fiction.mp4"
+FILEURL="https://docs.google.com/uc?export=download&id=$FILEID"
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate ${FILEURL} -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=$FILEID" -O $FILENAME && rm -rf /tmp/cookies.txt
+
+# download pulp_fiction_audio.mp3
+FILEID="19LhfHev-QvszCwhQp-1TWjI90zpxGprM"
+FILENAME="pulp_fiction.mp4"
+FILEURL="https://docs.google.com/uc?export=download&id=$FILEID"
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate ${FILEURL} -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=$FILEID" -O $FILENAME && rm -rf /tmp/cookies.txt
+
+# ending download, back to previous folder
+cd ..
 
 # set virtualenv
 python3 -m pip install virtualenv
