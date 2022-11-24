bdce911ba69d448500d6555213c5ac11aa4d5cea
Jeyhun Abbasov <abbcyhn@gmail.com>
2022-11-24 06:57:19 +0200

    Update app2_server.py

--- app2/app2_client/app2_client.py
+++ app2/app2_client/app2_client.py
@@ -4,6 +4,9 @@ import json
 import requests
 from pydub import AudioSegment
 
+
+
+###################################################################### CONSTANTS
 INPUT_PATH = "input"
 AUDIO_INPUT_PATH = f"{INPUT_PATH}/audio.mp3"
 SUBTITLE_INPUT_PATH = f"{INPUT_PATH}/subtitle.txt"
@@ -14,6 +17,46 @@ SUBTITLE_OUTPUT_PATH = f"{OUTPUT_PATH}/subtitle.srt"
 FOG_API_ENDPOINT = "http://localhost:5000/api/sync"
 CLOUD_API_ENDPOINT = "http://localhost:5000/api/sync"
 
+
+###################################################################### AUDIO SLICING
+def split_audios():
+	audio = AudioSegment.from_mp3(AUDIO_INPUT_PATH)
+	audio_slices = audio[::10000]
+
+	slice_counter = 0
+	for index, chunk in enumerate(audio_slices):
+		# write audio slice
+		audio_slice_path = f"{INPUT_PATH}/audio_{index}.mp3"	
+		with open(audio_slice_path, "wb") as f:
+			chunk.export(f, format="mp3")
+		
+		slice_counter += 1
+	
+	return slice_counter
+
+
+###################################################################### SUBTITLE SLICING
+def readsubtitles(filepath):
+	with open(filepath, 'r') as f:
+		lines = f.readlines()
+	return lines
+
+def split_subtitles(slice_counter):
+	subtitle_slices = split_subtitles_helper(readsubtitles(SUBTITLE_INPUT_PATH), slice_counter)
+
+	for index in slice_counter:
+		# write subtitle slice
+		subtitle_slice_path = f"{INPUT_PATH}/subtitle_{index}.txt"
+		with open(subtitle_slice_path, "w") as f:
+			for subtitle in subtitle_slices[index]:
+				f.write(subtitle)
+
+def split_subtitles_helper(a, n):
+    k, m = divmod(len(a), n)
+    return (a[i*k+min(i, m):(i+1)*k+min(i+1, m)] for i in range(n))
+
+
+###################################################################### SYNC PART
 def sendto(audio_path, subtitle_path, api_endpoint):
 	files = {
 		'audio': open(audio_path, 'rb'),
@@ -22,14 +65,12 @@ def sendto(audio_path, subtitle_path, api_endpoint):
 	response = requests.post(url = api_endpoint, files=files)
 	return response.content
 
-def split_subtitles(a, n):
-    k, m = divmod(len(a), n)
-    return (a[i*k+min(i, m):(i+1)*k+min(i+1, m)] for i in range(n))
 
-def readlines(filepath):
-	with open(filepath, 'r') as f:
-		lines = f.readlines()
-	return lines
+###################################################################### OUTPUT MERGING
+def get_as_list(jsonfile):
+	jsonfile = jsonfile.decode('utf8').replace("'", '"')
+	myjson = json.loads(jsonfile)
+	return myjson['fragments']
 
 def change_format(sec, offset):
 	sec = sec.split('.')
@@ -37,11 +78,6 @@ def change_format(sec, offset):
 	ms = sec[1]
 	st = f"{time.strftime('%H:%M:%S', time.gmtime(s))},{ms}"
 	return st
-
-def get_as_list(jsonfile):
-	jsonfile = jsonfile.decode('utf8').replace("'", '"')
-	myjson = json.loads(jsonfile)
-	return myjson['fragments']
 
 def convert_to_subtitles(line_counter, jsonfile, offset, filepath):
 	l = get_as_list(jsonfile)
@@ -61,25 +97,20 @@ def convert_to_subtitles(line_counter, jsonfile, offset, filepath):
 	
 	return line_counter
 
+
 def main():
-	audio = AudioSegment.from_mp3(AUDIO_INPUT_PATH)
-	audio_slices = audio[::10000]
-	subtitle_slices = split_subtitles(readlines(SUBTITLE_INPUT_PATH), len(list(audio_slices)))
+	slice_counter = split_audios()
+	split_audios(slice_counter)
 
 	line_counter = 0
-	for index, chunk in enumerate(audio_slices):
+	for index in slice_counter:
 		response = "EMPTY RESPONSE"
 
-		# read audio slice
-		audio_slice_path = f"{INPUT_PATH}/audio_{index}.mp3"	
-		with open(audio_slice_path, "wb") as f:
-			chunk.export(f, format="mp3")
+		# audio slice
+		audio_slice_path = f"{INPUT_PATH}/audio_{index}.mp3"
 
-		# read subtitle slice
+		# subtitle slice
 		subtitle_slice_path = f"{INPUT_PATH}/subtitle_{index}.txt"
-		with open(subtitle_slice_path, "w") as f:
-			for subtitle in subtitle_slices[index]:
-				f.write(subtitle)
 
 		# send to fog
 		if (index % 2 == 0):
@@ -92,6 +123,7 @@ def main():
 			response = sendto(audio_slice_path, subtitle_slice_path, CLOUD_API_ENDPOINT)
 			print("RETRIEVING FROM CLOUD")
 
+		# merging
 		offset = index * 10
 		line_counter = convert_to_subtitles(line_counter, response, offset, SUBTITLE_OUTPUT_PATH)
 
